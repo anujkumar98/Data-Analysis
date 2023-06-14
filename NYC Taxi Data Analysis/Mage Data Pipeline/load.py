c3483b98c0e2ffa1d7dbb5c3a7a7e38290755cf2
anujkumar98 <kumar.anuj@northeastern.edu>
2023-06-14 17:09:40 -0400

    Update the files for NYC Taxi Project

--- NYC Taxi Data Analysis/Mage Data Pipeline/load.py
+++ NYC Taxi Data Analysis/Mage Data Pipeline/load.py
@@ -12,11 +12,19 @@ def load_data_from_api(*args, **kwargs):
     """
     Template for loading data from API
     """
-    url = 'https://storage.googleapis.com/nyc-taxi-data-covid/nyc_taxi_data.csv'
-    response = requests.get(url)
+    # url = 'https://storage.googleapis.com/nyc-taxi-data-covid/nyc_taxi_data.csv'
+    # response = requests.get(url)
 
-    return pd.read_csv(io.StringIO(response.text), sep=',')
-
+    # return pd.read_csv(io.StringIO(response.text), sep=',')
+    df=pd.DataFrame()
+    base_url="https://storage.googleapis.com/nyc-taxi-data-covid/data/"
+    for i in range(2019,2022):
+        for j in range(1,13):
+            url=base_url+str(i)+"/yellow_tripdata_"+str(i)+"-"+f"{j:02}"+".parquet"
+            df_new=pd.read_parquet(url)
+            df=pd.concat([df_new])
+    df.head()
+    return df
 
 @test
 def test_output(output, *args) -> None:
